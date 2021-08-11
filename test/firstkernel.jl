ccd4e6e86043911aee30108b7882987155d9a2db
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-11 12:50:21 +0200

    Set dependencies.

--- test/firstkernel.jl
+++ test/firstkernel.jl
@@ -1,6 +1,6 @@
 using MicroHH
 
-settings = Dict(
+settings_grid = Dict(
     "itot" => 32,
     "jtot" => 32,
     "ktot" => 32,
@@ -9,7 +9,16 @@ settings = Dict(
     "ysize" => 3200.,
     "zsize" => 3200.)
 
-grid = MicroHH.Grid(settings)
+settings_time = Dict(
+    "start_time" => 0.,
+    "end_time" => 7200.,
+    "dt" => 5. )
+
+settings = Dict(
+    "grid" => settings_grid,
+    "time" => settings_time)
+
+grid = MicroHH.Grid(settings["grid"])
 fields = MicroHH.Fields(grid)
 
 MicroHH.advection_diffusion!(fields, grid)
