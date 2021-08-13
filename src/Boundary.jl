3895dd97fe25cc96b4c95344fc25c6a0b35717d5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-14 00:00:57 +0200

    Added cyclic routines.

--- src/Boundary.jl
+++ src/Boundary.jl
@@ -28,13 +28,24 @@ function Boundary(d::Dict)
         s_top_type)
 end
 
+function boundary_cyclic_kernel!(
+    a, is, ie, js, je, igc, jgc)
+    # East-west BCs
+    @inbounds @. a[1:igc, :, :] = a[ie-igc+1:ie, :, :]
+    @inbounds @. a[ie+1:end, :, :] = a[is:is+igc-1, :, :]
+
+    # North-south BCs
+    @inbounds @. a[:, 1:jgc, :] = a[:, je-jgc+1:je, :]
+    @inbounds @. a[:, je+1:end, :] = a[:, js:js+igc-1, :]
+end
+
 function set_ghost_cells_bot_kernel!(
     a, a_bot, a_gradbot, dz, ks, bot_type::Boundary_type)
 
     if bot_type == Dirichlet::Boundary_type
         @inbounds @. a[:, :, ks-1] = 2a_bot[:, :] - a[:, :, ks]
     elseif bot_type == Neumann::Boundary_type
-        @inbounds @. a[:, :, ks-1] = a_gradbot[:, :] + a[:, :, ks]
+        @inbounds @. a[:, :, ks-1] = -a_gradbot[:, :]*dz + a[:, :, ks]
     end
 end
 
@@ -49,6 +60,16 @@ function set_ghost_cells_top_kernel!(
 end
 
 function set_boundary!(fields::Fields, grid::Grid, boundary::Boundary)
+    # Cyclic BC.
+    boundary_cyclic_kernel!(
+        fields.u, grid.is, grid.ie, grid.js, grid.je, grid.igc, grid.jgc)
+    boundary_cyclic_kernel!(
+        fields.v, grid.is, grid.ie, grid.js, grid.je, grid.igc, grid.jgc)
+    boundary_cyclic_kernel!(
+        fields.w, grid.is, grid.ie, grid.js, grid.je, grid.igc, grid.jgc)
+    boundary_cyclic_kernel!(
+        fields.s, grid.is, grid.ie, grid.js, grid.je, grid.igc, grid.jgc)
+    
     # Bottom BC.
     set_ghost_cells_bot_kernel!(
         fields.u, fields.u_bot, fields.u_gradbot, grid.dz, grid.ks, boundary.mom_bot_type)
