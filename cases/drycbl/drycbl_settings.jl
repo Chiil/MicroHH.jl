f77e3e24a2cde60aca8a4d86e7b5cd1c820b7afd
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-17 23:13:10 +0200

    Running test case at 96^3.

--- cases/drycbl/drycbl_settings.jl
+++ cases/drycbl/drycbl_settings.jl
@@ -1,27 +1,27 @@
 ## Model settings.
 settings_grid = Dict(
-    "itot" => 32,
-    "jtot" => 32,
-    "ktot" => 32,
+    "itot" => 96,
+    "jtot" => 96,
+    "ktot" => 96,
 
     "xsize" => 3200.,
     "ysize" => 3200.,
     "zsize" => 3200.)
 
 settings_fields = Dict(
-    "visc" => 1.)
+    "visc" => 5.)
 
 settings_boundary = Dict(
     "mom_bot_type" => "Dirichlet",
     "mom_top_type" => "Neumann",
-    "s_bot_type" => "Dirichlet",
+    "s_bot_type" => "Neumann",
     "s_top_type" => "Neumann")
 
 settings_timeloop = Dict(
     "start_time" => 0.,
     "end_time" => 7200.,
     "save_time" => 1800.,
-    "dt" => 4.)
+    "dt" => 5.)
 
 settings = Dict(
     "grid"     => settings_grid,
