858e87249504e7c28ca3f5c3970a20ca751a79e2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-17 21:12:39 +0200

    Added first test case.

---
+++ cases/drycbl/drycbl_init.jl
@@ -0,0 +1,24 @@
+## Loading packages.
+using MicroHH
+using Tullio
+using Statistics
+
+
+## Loading settings.
+include("drycbl_settings.jl")
+
+
+## Create the initials fields.
+f = model.fields; g = model.grid
+s = @view f.s[g.is:g.ie, g.js:g.je, g.ks:g.ke]
+z = range(g.dz[1]/2, g.zsize, step=g.dz[1]) |> collect
+rand2d = rand(g.itot, g.jtot)
+rand2d .-= mean(rand2d)
+s[:, :, 1] .+= rand2d[:, :]
+f.s_gradbot[:, :] .= - 0.1 / settings["fields"]["visc"]
+@tullio s[i, j, k] += 0.003 * z[k]
+
+
+## Save the restart files.
+save_model!(model)
+
---
+++ cases/drycbl/drycbl_settings.jl
@@ -0,0 +1,31 @@
+## Model settings.
+settings_grid = Dict(
+    "itot" => 32,
+    "jtot" => 32,
+    "ktot" => 32,
+
+    "xsize" => 3200.,
+    "ysize" => 3200.,
+    "zsize" => 3200.)
+
+settings_fields = Dict(
+    "visc" => 1.)
+
+settings_boundary = Dict(
+    "mom_bot_type" => "Dirichlet",
+    "mom_top_type" => "Neumann",
+    "s_bot_type" => "Dirichlet",
+    "s_top_type" => "Neumann")
+
+settings_timeloop = Dict(
+    "start_time" => 0.,
+    "end_time" => 7200.,
+    "save_time" => 1800.,
+    "dt" => 1.)
+
+settings = Dict(
+    "grid"     => settings_grid,
+    "fields"   => settings_fields,
+    "boundary" => settings_boundary,
+    "timeloop" => settings_timeloop)
+
