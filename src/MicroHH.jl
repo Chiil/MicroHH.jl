bd276639ea952e3bc3b69d99a2d6c687a9e385ee
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-19 15:58:24 +0200

    More compact variable names in main routine.

--- src/MicroHH.jl
+++ src/MicroHH.jl
@@ -40,23 +40,23 @@ function Model(name, settings::Dict)
     Model(name, grid, fields, boundary, timeloop, pressure, 0)
 end
 
-function calc_rhs!(model::Model)
-    set_boundary!(model.fields, model.grid, model.boundary)
-    calc_dynamics_tend!(model.fields, model.grid)
-    calc_pressure_tend!(model.fields, model.grid, model.timeloop, model.pressure)
+function calc_rhs!(m::Model)
+    set_boundary!(m.fields, m.grid, m.boundary)
+    calc_dynamics_tend!(m.fields, m.grid)
+    calc_pressure_tend!(m.fields, m.grid, m.timeloop, m.pressure)
 end
 
-function prepare_model!(model::Model)
-    calc_rhs!(model)
-    model.last_measured_time[] = time_ns()
-    check_model(model)
+function prepare_model!(m::Model)
+    calc_rhs!(m)
+    m.last_measured_time[] = time_ns()
+    check_model(m)
 end
 
-function save_model(model::Model)
-    f = model.fields
-    g = model.grid
+function save_model(m::Model)
+    f = m.fields
+    g = m.grid
 
-    filename = @sprintf("%s.%02i.%08i.h5", model.name, 1, round(model.timeloop.time))
+    filename = @sprintf("%s.%02i.%08i.h5", m.name, 1, round(m.timeloop.time))
     h5open(filename, "w") do fid
         write(fid, "u", f.u[g.is:g.ie, g.js:g.je, g.ks:g.ke ])
         write(fid, "v", f.v[g.is:g.ie, g.js:g.je, g.ks:g.ke ])
@@ -67,11 +67,11 @@ function save_model(model::Model)
     end
 end
 
-function load_model!(model::Model)
-    f = model.fields
-    g = model.grid
+function load_model!(m::Model)
+    f = m.fields
+    g = m.grid
 
-    filename = @sprintf("%s.%02i.%08i.h5", model.name, 1, round(model.timeloop.time))
+    filename = @sprintf("%s.%02i.%08i.h5", m.name, 1, round(m.timeloop.time))
     h5open(filename, "r") do fid
         f.u[g.is:g.ie, g.js:g.je, g.ks:g.ke ] = read(fid, "u")
         f.v[g.is:g.ie, g.js:g.je, g.ks:g.ke ] = read(fid, "v")
@@ -82,36 +82,36 @@ function load_model!(model::Model)
     end
 end
 
-function step_model!(model::Model)
-    time_next = model.timeloop.time + model.timeloop.dt
+function step_model!(m::Model)
+    time_next = m.timeloop.time + m.timeloop.dt
 
-    while (model.timeloop.time < time_next)
-        integrate_time!(model.fields, model.grid, model.timeloop)
-        step_time!(model.timeloop)
+    while (m.timeloop.time < time_next)
+        integrate_time!(m.fields, m.grid, m.timeloop)
+        step_time!(m.timeloop)
 
-        if (isapprox(model.timeloop.time % model.timeloop.save_time, 0.)
-            && model.timeloop.rkstep == 1 && !isapprox(model.timeloop.time, model.timeloop.start_time))
-            save_model(model)
+        if (isapprox(m.timeloop.time % m.timeloop.save_time, 0.)
+            && m.timeloop.rkstep == 1 && !isapprox(m.timeloop.time, m.timeloop.start_time))
+            save_model(m)
         end
 
-        calc_rhs!(model)
+        calc_rhs!(m)
     end
 
-    if isapprox(model.timeloop.time % model.timeloop.check_time, 0.)
-        check_model(model)
+    if isapprox(m.timeloop.time % m.timeloop.check_time, 0.)
+        check_model(m)
     end
 
-    return model.timeloop.time < model.timeloop.end_time
+    return m.timeloop.time < m.timeloop.end_time
 end
 
-function check_model(model::Model)
-    old_time = model.last_measured_time[]
-    model.last_measured_time[] = time_ns()
+function check_model(m::Model)
+    old_time = m.last_measured_time[]
+    m.last_measured_time[] = time_ns()
     status_string = @sprintf("(%11.2f) Time/iter = %8.3f, Div = %6.3E, CFL = %6.3f",
-        model.timeloop.time,
-        (model.last_measured_time[] - old_time) * 1e-9 * model.timeloop.dt / model.timeloop.check_time,
-        calc_divergence(model.fields, model.grid),
-        calc_cfl(model.fields, model.grid, model.timeloop))
+        m.timeloop.time,
+        (m.last_measured_time[] - old_time) * 1e-9 * m.timeloop.dt / m.timeloop.check_time,
+        calc_divergence(m.fields, m.grid),
+        calc_cfl(m.fields, m.grid, m.timeloop))
 
     println(status_string)
 end
