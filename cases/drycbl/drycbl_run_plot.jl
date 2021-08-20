85a0723983d99a883195ac29659c662cf0ca977f
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-20 17:11:19 +0200

    Real time plotting, this time with the file saved. Very slow.

--- cases/drycbl/drycbl_run_plot.jl
+++ cases/drycbl/drycbl_run_plot.jl
@@ -2,7 +2,7 @@
 using MicroHH
 using Tullio
 using Statistics
-# using Profile
+using GLMakie
 
 
 ## Loading settings.
@@ -12,17 +12,14 @@ include("drycbl_settings.jl")
 ## Initialize the model.
 n_domains = 1
 m = Model("drycbl", n_domains, settings)
-
-
-## Load the restart data.
 load_model!(m)
+prepare_model!(m)
+heatmap(m.domains[1].fields.s[:, :, 2])
 
 
 ## Run the model.
-prepare_model!(m)
-
 in_progress = true
 while in_progress
     global in_progress = step_model!(m)
-    # @profile global in_progress = step_model!(m)
+    heatmap!(m.domains[1].fields.s[:, :, 2])
 end
