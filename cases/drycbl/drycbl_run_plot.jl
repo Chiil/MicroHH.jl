c5b6b26f09b52262898ce32432fb8aecc7f20432
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-08-20 16:56:59 +0200

    Added realtime plotting script, very slow still.

---
+++ cases/drycbl/drycbl_run_plot.jl
@@ -0,0 +1,28 @@
+## Loading packages.
+using MicroHH
+using Tullio
+using Statistics
+# using Profile
+
+
+## Loading settings.
+include("drycbl_settings.jl")
+
+
+## Initialize the model.
+n_domains = 1
+m = Model("drycbl", n_domains, settings)
+
+
+## Load the restart data.
+load_model!(m)
+
+
+## Run the model.
+prepare_model!(m)
+
+in_progress = true
+while in_progress
+    global in_progress = step_model!(m)
+    # @profile global in_progress = step_model!(m)
+end
