4fd08bece4b20bdf21f72348d6360d7f22110af5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-09-20 15:24:22 +0200

    TG script that computes 5 resolutions and plot convergence plot.

--- cases/taylorgreen/taylorgreen.jl
+++ cases/taylorgreen/taylorgreen.jl
@@ -6,118 +6,102 @@ using PyPlot
 pygui(:qt5)
 
 
-## Loading settings.
-d_grid = Dict{String, Any}()
-d_grid["itot"] = 256
-d_grid["jtot"] = 1
-d_grid["ktot"] = 128
+## Solve the Taylor Green vortex
+function solve_taylorgreen(itot)
+    # Loading settings.
+    d_grid = Dict{String, Any}()
+    d_grid["itot"] = itot
+    d_grid["jtot"] = 1
+    d_grid["ktot"] = itot ÷ 2
 
-d_grid["xsize"] = 1.
-d_grid["ysize"] = 1.
-d_grid["zsize"] = 0.5
+    d_grid["xsize"] = 1.
+    d_grid["ysize"] = 1.
+    d_grid["zsize"] = 0.5
 
-zsize = d_grid["zsize"]; ktot = d_grid["ktot"]
-dz = zsize / ktot
-d_grid["z"] = range(0.5*dz, step=dz, length=ktot) |> collect
+    zsize = d_grid["zsize"]; ktot = d_grid["ktot"]
+    dz = zsize / ktot
+    d_grid["z"] = range(0.5*dz, step=dz, length=ktot) |> collect
 
-d_fields = Dict{String, Any}()
-d_fields["visc"] = (8 * pi^2 * 100)^(-1)
-d_fields["alpha"] = 0.
+    d_fields = Dict{String, Any}()
+    d_fields["visc"] = (8 * pi^2 * 100)^(-1)
+    d_fields["alpha"] = 0.
 
-d_boundary = Dict{String, Any}()
-d_boundary["mom_bot_type"] = "Neumann"
-d_boundary["mom_top_type"] = "Neumann"
-d_boundary["s_bot_type"] = "Neumann"
-d_boundary["s_top_type"] = "Neumann"
+    d_boundary = Dict{String, Any}()
+    d_boundary["mom_bot_type"] = "Neumann"
+    d_boundary["mom_top_type"] = "Neumann"
+    d_boundary["s_bot_type"] = "Neumann"
+    d_boundary["s_top_type"] = "Neumann"
 
-d_timeloop = Dict{String, Any}()
-d_timeloop["start_time"] = 0.
-d_timeloop["end_time"] = 1.
-d_timeloop["save_time"] = 100.
-d_timeloop["check_time"] = 0.1
-d_timeloop["dt"] = 0.001
+    d_timeloop = Dict{String, Any}()
+    d_timeloop["start_time"] = 0.
+    d_timeloop["end_time"] = 1.
+    d_timeloop["save_time"] = 100.
+    d_timeloop["check_time"] = 0.1
+    d_timeloop["dt"] = 0.001
 
-settings = [ Dict(
-    "grid"     => d_grid,
-    "fields"   => d_fields,
-    "boundary" => d_boundary,
-    "timeloop" => d_timeloop) ]
-
-
-## Initialize the model.
-n_domains = 1
-m = Model("taylorgreen", n_domains, settings, Float64)
+    settings = [ Dict(
+        "grid"     => d_grid,
+        "fields"   => d_fields,
+        "boundary" => d_boundary,
+        "timeloop" => d_timeloop) ]
 
 
-## Create the initials fields.
-f = m.fields[1]; g = m.grid[1]
-u = @view f.u[g.is:g.ie, g.js:g.je, g.ks:g.ke]
-w = @view f.w[g.is:g.ie, g.js:g.je, g.ks:g.keh]
-p = @view f.p[g.is:g.ie, g.js:g.je, g.ks:g.ke]
-x = @view g.x[g.is:g.ie]
-xh = @view g.xh[g.is:g.ie]
-z = @view g.z[g.ks:g.ke]
-zh = @view g.zh[g.ks:g.keh]
-@tullio u[i, j, k] = sin(2pi*xh[i])*cos(2pi*z[k])
-@tullio w[i, j, k] = -cos(2pi*x[i])*sin(2pi*zh[k])
-
-f_t = exp(-8. * pi^2 * d_fields["visc"] * d_timeloop["end_time"])
-u_ref = u[:, :, :] * f_t
-w_ref = w[:, :, :] * f_t
-p_ref = zeros(size(u_ref))
-@tullio p_ref[i, j, k] = (1/4 * (cos(4pi*x[i]) + cos(4pi*z[k])) - 1/4) * f_t^2
+    # Initialize the model.
+    n_domains = 1
+    m = Model("taylorgreen", n_domains, settings, Float64)
 
 
-## Run the model
-in_progress = prepare_model!(m)
-while in_progress
-    global in_progress = step_model!(m)
+    # Create the initials fields.
+    f = m.fields[1]; g = m.grid[1]
+    u = @view f.u[g.is:g.ie, g.js:g.je, g.ks:g.ke]
+    w = @view f.w[g.is:g.ie, g.js:g.je, g.ks:g.keh]
+    p = @view f.p[g.is:g.ie, g.js:g.je, g.ks:g.ke]
+    x = @view g.x[g.is:g.ie]
+    xh = @view g.xh[g.is:g.ie]
+    z = @view g.z[g.ks:g.ke]
+    zh = @view g.zh[g.ks:g.keh]
+    @tullio u[i, j, k] = sin(2pi*xh[i])*cos(2pi*z[k])
+    @tullio w[i, j, k] = -cos(2pi*x[i])*sin(2pi*zh[k])
+
+    f_t = exp(-8. * pi^2 * d_fields["visc"] * d_timeloop["end_time"])
+    u_ref = u[:, :, :] * f_t
+    w_ref = w[:, :, :] * f_t
+    p_ref = zeros(size(u_ref))
+    @tullio p_ref[i, j, k] = (1/4 * (cos(4pi*x[i]) + cos(4pi*z[k])) - 1/4) * f_t^2
+
+
+    # Run the model
+    in_progress = prepare_model!(m)
+    while in_progress
+        in_progress = step_model!(m)
+    end
+
+    dx = 1. / itot
+    u_err = sum(dx^2 * (u .- u_ref).^2)
+    w_err = sum(dx^2 * (w .- w_ref).^2)
+    p_err = sum(dx^2 * (p .- p_ref).^2)
+
+    return u_err, w_err, p_err
 end
 
 
-## Plot the end result.
-u_err = u .- u_ref
-w_err = w .- w_ref
-p_err = p .- p_ref
-println(maximum(abs.(u_err)))
-println(maximum(abs.(w_err)))
-println(maximum(abs.(p_err)))
+## Solve the case at multiple resolutions.
+u016, w016, p016 = solve_taylorgreen(16)
+u032, w032, p032 = solve_taylorgreen(32)
+u064, w064, p064 = solve_taylorgreen(64)
+u128, w128, p128 = solve_taylorgreen(128)
+u256, w256, p256 = solve_taylorgreen(256)
 
-figure()
-subplot(131)
-pcolormesh(xh, z, u[:, 1, :]')
-colorbar()
-subplot(132)
-pcolormesh(xh, z, u_ref[:, 1, :]')
-colorbar()
-subplot(133)
-pcolormesh(xh, z, u_err[:, 1, :]')
-colorbar()
-tight_layout()
-display(gcf())
+dxs = [ 1/16, 1/32, 1/64, 1/128, 1/256 ]
+u_errs = [ u016, u032, u064, u128, u256 ]
+w_errs = [ w016, w032, w064, w128, w256 ]
+p_errs = [ p016, p032, p064, p128, p256 ]
 
-figure()
-subplot(131)
-pcolormesh(x, zh, w[:, 1, :]')
-colorbar()
-subplot(132)
-pcolormesh(x, zh, w_ref[:, 1, :]')
-colorbar()
-subplot(133)
-pcolormesh(x, zh, w_err[:, 1, :]')
-colorbar()
-tight_layout()
-display(gcf())
 
+## Plot the results
 figure()
-subplot(131)
-pcolormesh(x, z, p[:, 1, :]')
-colorbar()
-subplot(132)
-pcolormesh(x, z, p_ref[:, 1, :]')
-colorbar()
-subplot(133)
-pcolormesh(x, z, p_err[:, 1, :]')
-colorbar()
-tight_layout()
+loglog(dxs, u_errs, "C0o-", label="u")
+loglog(dxs, w_errs, "C1o-", label="w")
+loglog(dxs, p_errs, "C2o-", label="p")
+legend(loc=0, frameon=False)
 display(gcf())
