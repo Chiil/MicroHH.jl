a4cc60a34a44cdb942e99025a8e559ec398778b5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-09-22 13:17:26 +0200

    Plot two variables.

--- cases/drycbl/drycbl_run_plot.jl
+++ cases/drycbl/drycbl_run_plot.jl
@@ -14,24 +14,25 @@ n_domains = 1
 m = Model("drycbl", n_domains, settings, Float32)
 load_model!(m)
 in_progress = prepare_model!(m)
+
 f = m.fields[1]; g = m.grid[1]
 x = @view g.x[g.is:g.ie]
 y = @view g.y[g.js:g.je]
 z = @view g.z[g.ks:g.ke]
-s = @view f.s[g.is:g.ie, g.js:g.je, g.ks:g.ke]
 s_bot = @view f.s_bot[g.is:g.ie, g.js:g.je]
+s = @view f.s[g.is:g.ie, g.js:g.je, g.ks:g.ke]
 
-fig = Figure(resolution=(500, 800))
+fig = Figure(resolution=(800, 500))
 node1 = Node(s_bot .- mean(s_bot))
 h1 = heatmap(fig[1, 1], x, y, node1, colorrange=(-0.4, 0.4))
-
-fig = Figure(resolution=(500, 800))
-node2 = Node(s_bot .- mean(s_bot))
+node2 = Node(s[:, 1, :] .- mean(s, dims=(1, 2))[:, 1, :])
 h2 = heatmap(fig[1, 2], x, z, node2, colorrange=(-0.4, 0.4))
+fig
 
 
 ## Run the model.
 while in_progress
     global in_progress = step_model!(m)
-    node[] = s_bot .- mean(s_bot)
+    node1[] = s_bot .- mean(s_bot)
+    node2[] = s[:, 1, :] .- mean(s, dims=(1, 2))[:, 1, :]
 end
