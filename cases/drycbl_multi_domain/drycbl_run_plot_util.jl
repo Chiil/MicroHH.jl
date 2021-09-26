301423cc053641f57a5a8dd262f75affacb9045c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-09-26 21:19:07 +0200

    Do not transfer state by default when extending the run.

--- cases/drycbl_multi_domain/drycbl_run_plot_util.jl
+++ cases/drycbl_multi_domain/drycbl_run_plot_util.jl
@@ -8,7 +8,6 @@ node2[] = s_bot2 .- mean(s_bot2)
 
 
 ## Run the model for another 900 sec.
-MicroHH.transfer_state!(f1, f2, g1, g2)
 m.timeloop[1].end_time += 900.
 m.timeloop[1].iend_time += 900 * MicroHH.ifactor
 in_progress = true
