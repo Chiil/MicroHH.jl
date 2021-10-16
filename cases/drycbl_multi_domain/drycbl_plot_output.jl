2bdec96cb62af2bfad2f0a12784d4d457070452e
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-16 13:42:57 +0200

    Added additional interpolation plot to plot set.

--- cases/drycbl_multi_domain/drycbl_plot_output.jl
+++ cases/drycbl_multi_domain/drycbl_plot_output.jl
@@ -2,38 +2,40 @@
 using PyPlot
 using HDF5
 using Statistics
+using Interpolations
 
 
 ## Read the data.
-x_01 = h5read("drycbl.01.00005400.h5", "x")
-x_02 = h5read("drycbl.02.00005400.h5", "x")
-y_01 = h5read("drycbl.01.00005400.h5", "y")
-y_02 = h5read("drycbl.02.00005400.h5", "y")
-zh_01 = h5read("drycbl.01.00005400.h5", "zh")
-zh_02 = h5read("drycbl.02.00005400.h5", "zh")
+x_01 = h5read("drycbl.01.00007200.h5", "x")
+x_02 = h5read("drycbl.02.00007200.h5", "x")
+y_01 = h5read("drycbl.01.00007200.h5", "y")
+y_02 = h5read("drycbl.02.00007200.h5", "y")
+zh_01 = h5read("drycbl.01.00007200.h5", "zh")
+zh_02 = h5read("drycbl.02.00007200.h5", "zh")
 
-s_bot_01 = h5read("drycbl.01.00005400.h5", "s_bot", (:, :))
-s_bot_02 = h5read("drycbl.02.00005400.h5", "s_bot", (:, :))
+s_bot_01 = h5read("drycbl.01.00007200.h5", "s_bot", (:, :))
+s_bot_02 = h5read("drycbl.02.00007200.h5", "s_bot", (:, :))
 
 s_bot_01 .-= mean(s_bot_01)
 s_bot_02 .-= mean(s_bot_02)
 
-w_xy_01 = h5read("drycbl.01.00005400.h5", "w", (:, :, 13)) # at 300 m
-w_xy_02 = h5read("drycbl.02.00005400.h5", "w", (:, :, 25))
+println(zh_01[17], " ", zh_02[49])
+w_xy_01 = h5read("drycbl.01.00007200.h5", "w", (:, :, 25)) # at 300 m
+w_xy_02 = h5read("drycbl.02.00007200.h5", "w", (:, :, 49))
 
-w_xz_01 = h5read("drycbl.01.00005400.h5", "w", (:, 1, :))
-w_xz_02 = h5read("drycbl.02.00005400.h5", "w", (:, 1, :))
+w_xz_01 = h5read("drycbl.01.00007200.h5", "w", (:, 1, :))
+w_xz_02 = h5read("drycbl.02.00007200.h5", "w", (:, 1, :))
 
 
 ## Surface temperature figure.
 figure(figsize=(9,4.5))
 subplot(121)
-pcolormesh(x_01, y_01, s_bot_01, vmin=-0.5, vmax=0.5, shading="nearest")
+pcolormesh(x_01, y_01, s_bot_01, vmin=-0.6, vmax=0.6, shading="nearest")
 xlabel("x (m)")
 ylabel("y (m)")
 
 subplot(122)
-pcolormesh(x_02, y_02, s_bot_02, vmin=-0.5, vmax=0.5, shading="nearest")
+pcolormesh(x_02, y_02, s_bot_02, vmin=-0.6, vmax=0.6, shading="nearest")
 xlabel("x (m)")
 ylabel("y (m)")
 
@@ -49,8 +51,8 @@ s_02 = copy(s_bot_02)
 @. s_02[s_02 < 0.1] = NaN
 
 figure(figsize=(5,4.5))
-pcolormesh(x_02, y_02, s_02, vmin=0., vmax=0.5, shading="nearest", cmap=PyPlot.cm.Reds, alpha=0.5)
-pcolormesh(x_01, y_01, s_01, vmin=0., vmax=0.5, shading="nearest", cmap=PyPlot.cm.Blues, alpha=0.5)
+pcolormesh(x_02, y_02, s_02, vmin=0., vmax=0.6, shading="nearest", cmap=PyPlot.cm.Reds, alpha=0.4)
+pcolormesh(x_01, y_01, s_01, vmin=0., vmax=0.6, shading="nearest", cmap=PyPlot.cm.Blues, alpha=0.4)
 xlabel("x (m)")
 ylabel("y (m)")
 
@@ -73,6 +75,25 @@ ylabel("y (m)")
 tight_layout()
 
 println(var(w_xy_01), " ", var(w_xy_02))
+
+
+## Vertical velocity xy interpolated
+interp = LinearInterpolation((x_02, y_02), w_xy_02, extrapolation_bc=Periodic())
+w_xy_02_to_01 = interp(x_01, y_01)
+println(size(w_xy_02_to_01))
+
+figure(figsize=(9,4.5))
+subplot(121)
+pcolormesh(x_01, y_01, w_xy_01, vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic, shading="nearest")
+xlabel("x (m)")
+ylabel("y (m)")
+
+subplot(122)
+pcolormesh(x_01, y_01, w_xy_02_to_01, vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic, shading="nearest")
+xlabel("x (m)")
+ylabel("y (m)")
+
+tight_layout()
 
 
 ## Vertical velocity xz
