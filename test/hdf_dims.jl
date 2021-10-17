0594776dee1d999682f2c662095a378168956691
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-17 14:42:02 +0200

    Added test script for dimensions in HDF5.

---
+++ test/hdf_dims.jl
@@ -0,0 +1,16 @@
+using HDF5
+
+filename = "test.h5"
+
+x = 0.:10:100 |> collect
+y = 0.:2:10 |> collect
+z = [ i^2 + j for i in x, j in y ]
+
+h5open(filename, "w") do fid
+    fid["x"] = x[:]
+    fid["y"] = y[:]
+    fid["z"] = z[:, :]
+    HDF5.h5ds_set_scale(fid["x"], "x")
+    HDF5.h5ds_set_scale(fid["y"], "y")
+    # HDF5.h5ds_attach_scale(fid["y"], fid["x"], 0)
+end
