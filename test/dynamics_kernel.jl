2f1ac708e6c2eda250ffec7141ced0e9ec72156b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-20 11:41:23 +0200

    Added precision to fftw and dynamics tests.

--- test/dynamics_kernel.jl
+++ test/dynamics_kernel.jl
@@ -1,9 +1,11 @@
-## Packages
+## Packages and settings.
 include("../src/StencilBuilder.jl")
 
 using BenchmarkTools
 using LoopVectorization
 using .StencilBuilder
+
+float_type = Float32
 
 
 ## CPU dynamics kernel.
@@ -32,16 +34,16 @@ igc = 1; jgc = 1; kgc = 1;
 icells = itot + 2igc; jcells = jtot + 2jgc; kcells = ktot + 2kgc
 is = igc+1; ie = igc+itot; js = jgc+1; je = jgc+jtot; ks = kgc+1; ke = kgc+ktot
 
-wt = rand(icells, jcells, kcells)
-u = rand(icells, jcells, kcells)
-v = rand(icells, jcells, kcells)
-w = rand(icells, jcells, kcells)
-s = rand(icells, jcells, kcells)
-dxi = rand()
-dyi = rand()
-dzi = rand(kcells)
-dzhi = rand(kcells)
-visc = 1.
+wt = rand(float_type, icells, jcells, kcells)
+u = rand(float_type, icells, jcells, kcells)
+v = rand(float_type, icells, jcells, kcells)
+w = rand(float_type, icells, jcells, kcells)
+s = rand(float_type, icells, jcells, kcells)
+dxi = rand(float_type)
+dyi = rand(float_type)
+dzi = rand(float_type, kcells)
+dzhi = rand(float_type, kcells)
+visc = convert(float_type, 1)
 
 
 ## Run CPU kernel
