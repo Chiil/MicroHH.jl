63ca86132ac876eafd0c3fcbd590b2027b31f8b5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-23 21:31:57 +0200

    linear interpolation works without edge.

--- test/refine_1d.jl
+++ test/refine_1d.jl
@@ -6,15 +6,24 @@ using PyPlot
 
 
 ## Create the refine function.
-function refine_field!(hi, lo)
+function refine_field_nn!(hi, lo)
     @tullio hi[2(i-1)+l] = lo[i] (l in 1:2)
     return
 end
 
+function refine_field_int!(hi, lo)
+    for i in 0:2:size(hi, dims=1)-3
+        i_lo = i÷2+1
+        i_hi = i+1
+        hi[i_hi  ] = 3/4*lo[i_lo] + 1/4*lo[i_lo-1]
+        hi[i_hi+1] = 3/4*lo[i_lo] + 1/4*lo[i_lo+1]
+    end
+end
+
 
 ## Set up the grids.
-n_hi = 64
-n_lo = 32
+n_hi = 32
+n_lo = 16
 
 a_lo = rand(n_lo)
 a_hi = zeros(n_hi)
@@ -33,11 +42,10 @@ a_lo_gc[end] = a_lo[2]
 
 ## Compute.
 a_hi_nn = copy(a_hi)
-refine_field!(a_hi_nn, a_lo)
-#refine_field!(a_hi_gc, a_lo_gc)
+refine_field_nn!(a_hi_nn, a_lo)
 
-#a_hi[:] = a_hi_gc[2:end-1]
-#a_lo[:] = a_lo_gc[2:end-1]
+refine_field_int!(a_hi_gc, a_lo_gc)
+a_hi_int = a_hi_gc[2:end-1]
 
 
 ## Check the output.
@@ -49,6 +57,7 @@ x_lo = dx_lo/2:dx_lo:1 |> collect
 
 figure()
 plot(x_hi, a_hi_nn, "C0-o")
+plot(x_hi, a_hi_int, "C1-o")
 plot(x_lo, a_lo, "k:")
 display(gcf())
 show()
