8dd37488c27841a99fa0712da254ad4ba775504b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-23 21:43:34 +0200

    Speed up computation, methods are fast.

--- test/refine_1d.jl
+++ test/refine_1d.jl
@@ -1,5 +1,6 @@
 ## Load the packages.
 using Tullio
+using LoopVectorization
 using Statistics
 using BenchmarkTools
 using PyPlot
@@ -12,7 +13,7 @@ function refine_field_nn!(hi, lo)
 end
 
 function refine_field_int!(hi, lo)
-    for i in 0:2:size(hi, 1)-3
+    @tturbo for i in 0:2:size(hi, 1)-3
         i_lo = i÷2+2
         i_hi = i+2
         hi[i_hi  ] = 3/4*lo[i_lo] + 1/4*lo[i_lo-1]
@@ -22,8 +23,8 @@ end
 
 
 ## Set up the grids.
-n_hi = 32
-n_lo = 16
+n_hi = 256
+n_lo = 128
 
 a_lo = rand(n_lo)
 a_hi = zeros(n_hi)
@@ -42,9 +43,9 @@ a_lo_gc[end] = a_lo_gc[2]
 
 ## Compute.
 a_hi_nn = copy(a_hi)
-refine_field_nn!(a_hi_nn, a_lo)
+@btime refine_field_nn!(a_hi_nn, a_lo)
 
-refine_field_int!(a_hi_gc, a_lo_gc)
+@btime refine_field_int!(a_hi_gc, a_lo_gc)
 a_hi_int = a_hi_gc[2:end-1]
 
 
