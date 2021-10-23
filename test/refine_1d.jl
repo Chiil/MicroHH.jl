bb5121395c5632a023b3041586344e27a0222571
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-10-23 22:02:21 +0200

    Working interpolation for 2 and 3 ratios

--- test/refine_1d.jl
+++ test/refine_1d.jl
@@ -7,24 +7,37 @@ using PyPlot
 
 
 ## Create the refine function.
-function refine_field_nn!(hi, lo)
-    @tullio hi[2(i-1)+l] = lo[i] (l in 1:2)
-    return
+function refine_field_nn!(hi, lo, n_hi, n_lo)
+    if n_hi ÷ n_lo == 2
+        @tullio hi[2(i-1)+l] = lo[i] (l in 1:2)
+    elseif n_hi ÷ n_lo == 3
+        @tullio hi[3(i-1)+l] = lo[i] (l in 1:3)
+    end
 end
 
-function refine_field_int!(hi, lo)
-    @tturbo for i in 0:2:size(hi, 1)-3
-        i_lo = i÷2+2
-        i_hi = i+2
-        hi[i_hi  ] = 3/4*lo[i_lo] + 1/4*lo[i_lo-1]
-        hi[i_hi+1] = 3/4*lo[i_lo] + 1/4*lo[i_lo+1]
+function refine_field_int!(hi, lo, n_hi, n_lo)
+    if n_hi ÷ n_lo == 2
+        @tturbo for i in 0:2:size(hi, 1)-3
+            i_lo = i÷2+2
+            i_hi = i+2
+            hi[i_hi  ] = 3//4*lo[i_lo] + 1//4*lo[i_lo-1]
+            hi[i_hi+1] = 3//4*lo[i_lo] + 1//4*lo[i_lo+1]
+        end
+    elseif n_hi ÷ n_lo == 3
+        @tturbo for i in 0:3:size(hi, 1)-3
+            i_lo = i÷3+2
+            i_hi = i+2
+            hi[i_hi  ] = 2//3*lo[i_lo] + 1//3*lo[i_lo-1]
+            hi[i_hi+1] = lo[i_lo]
+            hi[i_hi+2] = 2//3*lo[i_lo] + 1//3*lo[i_lo+1]
+        end
     end
 end
 
 
 ## Set up the grids.
-n_hi = 256
-n_lo = 128
+n_hi = 48
+n_lo = 16
 
 a_lo = rand(n_lo)
 a_hi = zeros(n_hi)
@@ -43,9 +56,9 @@ a_lo_gc[end] = a_lo_gc[2]
 
 ## Compute.
 a_hi_nn = copy(a_hi)
-@btime refine_field_nn!(a_hi_nn, a_lo)
+@btime refine_field_nn!(a_hi_nn, a_lo, n_hi, n_lo)
 
-@btime refine_field_int!(a_hi_gc, a_lo_gc)
+@btime refine_field_int!(a_hi_gc, a_lo_gc, n_hi, n_lo)
 a_hi_int = a_hi_gc[2:end-1]
 
 
