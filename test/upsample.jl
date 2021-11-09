145e54dd250020faaaf9dd93737b65dd60deda33
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-11-09 22:09:43 +0100

    Add new test for a generic upsampling routine

---
+++ test/upsample.jl
@@ -0,0 +1,61 @@
+## Load the packages.
+using LoopVectorization
+using Interpolations
+using OffsetArrays
+using Statistics
+using BenchmarkTools
+using PyPlot
+
+
+## Create the upsample functions.
+function upsample_field_int!()
+end
+
+
+function upsample_field_int_ref!(hi, lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
+    interp = interpolate((x_lo, y_lo, z_lo), lo, (Gridded(Constant()), Gridded(Constant()), Gridded(Constant())))
+    hi[2:end-1, 2:end-1, 2:end-1] .= interp(x_hi[2:end-1], y_hi[2:end-1], z_hi[2:end-1])
+end
+
+
+## Set up the grids.
+itot_lo = 4; jtot_lo = 8; ktot_lo = 2
+ifac = 2; jfac = 1; kfac = 1
+
+a_lo = rand(itot_lo + 2, jtot_lo + 2, ktot_lo + 2)
+a_hi = zeros(itot_lo*ifac + 2, jtot_lo*jfac + 2, ktot_lo*kfac + 2)
+
+
+dx_lo = 1/itot_lo; dy_lo = 1/jtot_lo; dz_lo = 1/ktot_lo
+x_lo = -dx_lo/2:dx_lo:1+dx_lo |> collect
+y_lo = -dy_lo/2:dy_lo:1+dy_lo |> collect
+z_lo = -dz_lo/2:dz_lo:1+dz_lo |> collect
+
+dx_hi = dx_lo/ifac; dy_hi = dy_lo/jfac; dz_hi = dz_lo/kfac
+x_hi = -dx_hi/2:dx_hi:1+dx_hi |> collect
+y_hi = -dy_hi/2:dy_hi:1+dy_hi |> collect
+z_hi = -dz_hi/2:dz_hi:1+dz_hi |> collect
+
+
+## Compute a reference using Interpolations.jl
+@btime upsample_field_int_ref!(a_hi, a_lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
+
+# println("Mean equal to lo: ", mean(a_lo) ≈ mean(a_hi_int))
+# println("Mean equal to ref: ", mean(a_hi_int_ref) ≈ mean(a_hi_int))
+# println("Interpolations equal: ", a_hi_int ≈ a_hi_int_ref)
+
+
+## Plot the output.
+xh_lo = 0:dx_lo:1 |> collect
+yh_lo = 0:dy_lo:1 |> collect
+xh_hi = 0:dx_hi:1 |> collect
+yh_hi = 0:dy_hi:1 |> collect
+
+figure(figsize=(10, 5))
+subplot(121)
+pcolormesh(xh_lo, yh_lo, a_lo[2:end-1, 2:end-1, 2]', vmin=0, vmax=1)
+subplot(122)
+pcolormesh(xh_hi, yh_hi, a_hi[2:end-1, 2:end-1, 2]', vmin=0, vmax=1)
+tight_layout()
+display(gcf())
+show()
