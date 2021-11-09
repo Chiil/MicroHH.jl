b14f402b0449427adc21f21301c1cd672f0264d5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-11-09 22:53:26 +0100

    Added checks, routine works perfectly

--- test/upsample.jl
+++ test/upsample.jl
@@ -8,18 +8,18 @@ using PyPlot
 
 
 ## Create the upsample functions.
-function upsample_field_int!(hi, lo, itot, jtot, ktot, ifac, jfac, kfac)
-       @tturbo for k in 0:ktot-1, j in 0:jtot-1, i in 0:itot-1
-            i_lo = i + 2; j_lo = j + 2; k_lo = k + 2
-            i_hi = ifac*i + 2; j_hi = jfac*j + 2; k_hi = kfac*k + 2
-            for kk in 0:kfac-1, jj in 0:jfac-1, ii in 0:ifac-1
-                hi[i_hi+ii, j_hi+jj, k_hi+kk] = lo[i_lo, j_lo, k_lo]
-            end
+function upsample_nn!(hi, lo, itot, jtot, ktot, ifac, jfac, kfac)
+    @tturbo for k in 0:ktot-1, j in 0:jtot-1, i in 0:itot-1
+        i_lo = i + 2; j_lo = j + 2; k_lo = k + 2
+        i_hi = ifac*i + 2; j_hi = jfac*j + 2; k_hi = kfac*k + 2
+        for kk in 0:kfac-1, jj in 0:jfac-1, ii in 0:ifac-1
+            hi[i_hi+ii, j_hi+jj, k_hi+kk] = lo[i_lo, j_lo, k_lo]
         end
+    end
 end
 
 
-function upsample_field_int_ref!(hi, lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
+function upsample_nn_ref!(hi, lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
     interp = interpolate((x_lo, y_lo, z_lo), lo, (Gridded(Constant()), Gridded(Constant()), Gridded(Constant())))
     hi[2:end-1, 2:end-1, 2:end-1] .= interp(x_hi[2:end-1], y_hi[2:end-1], z_hi[2:end-1])
 end
@@ -45,12 +45,16 @@ z_hi = -dz_hi/2:dz_hi:1+dz_hi |> collect
 
 
 ## Compute a reference using Interpolations.jl
-@btime upsample_field_int_ref!(a_hi_ref, a_lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
-@btime upsample_field_int!(a_hi, a_lo, itot_lo, jtot_lo, ktot_lo, ifac, jfac, kfac)
+@btime upsample_nn_ref!(a_hi_ref, a_lo, x_hi, y_hi, z_hi, x_lo, y_lo, z_lo)
+@btime upsample_nn!(a_hi, a_lo, itot_lo, jtot_lo, ktot_lo, ifac, jfac, kfac)
 
-# println("Mean equal to lo: ", mean(a_lo) ≈ mean(a_hi_int))
-# println("Mean equal to ref: ", mean(a_hi_int_ref) ≈ mean(a_hi_int))
-# println("Interpolations equal: ", a_hi_int ≈ a_hi_int_ref)
+a_lo_int = @view a_lo[2:end-1, 2:end-1, 2:end-1]
+a_hi_ref_int = @view a_hi_ref[2:end-1, 2:end-1, 2:end-1]
+a_hi_int = @view a_hi[2:end-1, 2:end-1, 2:end-1]
+
+println("Mean equal to lo: ", mean(a_lo_int) ≈ mean(a_hi_int))
+println("Mean equal to ref: ", mean(a_hi_ref_int) ≈ mean(a_hi_int))
+println("Values equal to ref: ", a_hi_int ≈ a_hi_ref_int)
 
 
 ## Plot the output.
@@ -59,7 +63,7 @@ yh_lo = 0:dy_lo:1 |> collect
 xh_hi = 0:dx_hi:1 |> collect
 yh_hi = 0:dy_hi:1 |> collect
 
-figure(figsize=(12, 5))
+figure(figsize=(12, 4))
 subplot(131)
 pcolormesh(xh_lo, yh_lo, a_lo[2:end-1, 2:end-1, 2]', vmin=0, vmax=1)
 subplot(132)
