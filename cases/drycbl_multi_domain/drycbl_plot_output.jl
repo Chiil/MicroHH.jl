4b4d80994fdf4aed7ef56e15ca840f3c58eff56f
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-11-21 18:21:15 +0100

    Further improve plots.

--- cases/drycbl_multi_domain/drycbl_plot_output.jl
+++ cases/drycbl_multi_domain/drycbl_plot_output.jl
@@ -6,34 +6,34 @@ using Interpolations
 
 
 ## Read the data.
-x_01 = h5read("drycbl.01.00001800.h5", "x")
-x_02 = h5read("drycbl.02.00001800.h5", "x")
-y_01 = h5read("drycbl.01.00001800.h5", "y")
-y_02 = h5read("drycbl.02.00001800.h5", "y")
-z_01 = h5read("drycbl.01.00001800.h5", "z")
-z_02 = h5read("drycbl.02.00001800.h5", "z")
-zh_01 = h5read("drycbl.01.00001800.h5", "zh")
-zh_02 = h5read("drycbl.02.00001800.h5", "zh")
+x_01 = h5read("drycbl.01.00002400.h5", "x")
+x_02 = h5read("drycbl.02.00002400.h5", "x")
+y_01 = h5read("drycbl.01.00002400.h5", "y")
+y_02 = h5read("drycbl.02.00002400.h5", "y")
+z_01 = h5read("drycbl.01.00002400.h5", "z")
+z_02 = h5read("drycbl.02.00002400.h5", "z")
+zh_01 = h5read("drycbl.01.00002400.h5", "zh")
+zh_02 = h5read("drycbl.02.00002400.h5", "zh")
 
-s_bot_01 = h5read("drycbl.01.00001800.h5", "s_bot", (:, :))
-s_bot_02 = h5read("drycbl.02.00001800.h5", "s_bot", (:, :))
+s_bot_01 = h5read("drycbl.01.00002400.h5", "s_bot", (:, :))
+s_bot_02 = h5read("drycbl.02.00002400.h5", "s_bot", (:, :))
 
 s_bot_01 .-= mean(s_bot_01)
 s_bot_02 .-= mean(s_bot_02)
 
 println(zh_01[13], " ", zh_02[37])
-w_xy_01 = h5read("drycbl.01.00001800.h5", "w", (:, :, 13)) # at 300 m
-w_xy_02 = h5read("drycbl.02.00001800.h5", "w", (:, :, 37))
+w_xy_01 = h5read("drycbl.01.00002400.h5", "w", (:, :, 13)) # at 300 m
+w_xy_02 = h5read("drycbl.02.00002400.h5", "w", (:, :, 37))
 
-w_xz_01 = h5read("drycbl.01.00001800.h5", "w", (:, 1, :))
-w_xz_02 = h5read("drycbl.02.00001800.h5", "w", (:, 1, :))
+w_xz_01 = h5read("drycbl.01.00002400.h5", "w", (:, 1, :))
+w_xz_02 = h5read("drycbl.02.00002400.h5", "w", (:, 1, :))
 
 
 ## Read for plotting.
-xh_01 = h5read("drycbl.01.00001800.h5", "xh")
-xh_02 = h5read("drycbl.02.00001800.h5", "xh")
-yh_01 = h5read("drycbl.01.00001800.h5", "yh")
-yh_02 = h5read("drycbl.02.00001800.h5", "yh")
+xh_01 = h5read("drycbl.01.00002400.h5", "xh")
+xh_02 = h5read("drycbl.02.00002400.h5", "xh")
+yh_01 = h5read("drycbl.01.00002400.h5", "yh")
+yh_02 = h5read("drycbl.02.00002400.h5", "yh")
 push!(xh_01, 3200.)
 push!(xh_02, 3200.)
 push!(yh_01, 3200.)
@@ -43,12 +43,12 @@ push!(yh_02, 3200.)
 ## Surface temperature figure.
 figure(figsize=(9,4.5))
 subplot(121)
-pcolormesh(xh_01, yh_01, s_bot_01', vmin=-0.6, vmax=0.6)
+pcolormesh(xh_01, yh_01, s_bot_01', vmin=-0.6, vmax=0.6, cmap=plt.cm.cividis)
 xlabel("x (m)")
 ylabel("y (m)")
 
 subplot(122)
-pcolormesh(xh_02, yh_02, s_bot_02', vmin=-0.6, vmax=0.6)
+pcolormesh(xh_02, yh_02, s_bot_02', vmin=-0.6, vmax=0.6, cmap=plt.cm.cividis)
 xlabel("x (m)")
 ylabel("y (m)")
 
