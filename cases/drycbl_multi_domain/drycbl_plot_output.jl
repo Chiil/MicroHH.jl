7c7b6ffa8b93bd25685ecc6bbbe041042d29a52c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2021-11-21 17:30:12 +0100

    Improved plotting script.

--- cases/drycbl_multi_domain/drycbl_plot_output.jl
+++ cases/drycbl_multi_domain/drycbl_plot_output.jl
@@ -6,34 +6,34 @@ using Interpolations
 
 
 ## Read the data.
-x_01 = h5read("drycbl.01.00007200.h5", "x")
-x_02 = h5read("drycbl.02.00007200.h5", "x")
-y_01 = h5read("drycbl.01.00007200.h5", "y")
-y_02 = h5read("drycbl.02.00007200.h5", "y")
-z_01 = h5read("drycbl.01.00007200.h5", "z")
-z_02 = h5read("drycbl.02.00007200.h5", "z")
-zh_01 = h5read("drycbl.01.00007200.h5", "zh")
-zh_02 = h5read("drycbl.02.00007200.h5", "zh")
+x_01 = h5read("drycbl.01.00001800.h5", "x")
+x_02 = h5read("drycbl.02.00001800.h5", "x")
+y_01 = h5read("drycbl.01.00001800.h5", "y")
+y_02 = h5read("drycbl.02.00001800.h5", "y")
+z_01 = h5read("drycbl.01.00001800.h5", "z")
+z_02 = h5read("drycbl.02.00001800.h5", "z")
+zh_01 = h5read("drycbl.01.00001800.h5", "zh")
+zh_02 = h5read("drycbl.02.00001800.h5", "zh")
 
-s_bot_01 = h5read("drycbl.01.00007200.h5", "s_bot", (:, :))
-s_bot_02 = h5read("drycbl.02.00007200.h5", "s_bot", (:, :))
+s_bot_01 = h5read("drycbl.01.00001800.h5", "s_bot", (:, :))
+s_bot_02 = h5read("drycbl.02.00001800.h5", "s_bot", (:, :))
 
 s_bot_01 .-= mean(s_bot_01)
 s_bot_02 .-= mean(s_bot_02)
 
-println(zh_01[17], " ", zh_02[33])
-w_xy_01 = h5read("drycbl.01.00007200.h5", "w", (:, :, 17)) # at 300 m
-w_xy_02 = h5read("drycbl.02.00007200.h5", "w", (:, :, 33))
+println(zh_01[13], " ", zh_02[37])
+w_xy_01 = h5read("drycbl.01.00001800.h5", "w", (:, :, 13)) # at 300 m
+w_xy_02 = h5read("drycbl.02.00001800.h5", "w", (:, :, 37))
 
-w_xz_01 = h5read("drycbl.01.00007200.h5", "w", (:, 1, :))
-w_xz_02 = h5read("drycbl.02.00007200.h5", "w", (:, 1, :))
+w_xz_01 = h5read("drycbl.01.00001800.h5", "w", (:, 1, :))
+w_xz_02 = h5read("drycbl.02.00001800.h5", "w", (:, 1, :))
 
 
 ## Read for plotting.
-xh_01 = h5read("drycbl.01.00007200.h5", "xh")
-xh_02 = h5read("drycbl.02.00007200.h5", "xh")
-yh_01 = h5read("drycbl.01.00007200.h5", "yh")
-yh_02 = h5read("drycbl.02.00007200.h5", "yh")
+xh_01 = h5read("drycbl.01.00001800.h5", "xh")
+xh_02 = h5read("drycbl.02.00001800.h5", "xh")
+yh_01 = h5read("drycbl.01.00001800.h5", "yh")
+yh_02 = h5read("drycbl.02.00001800.h5", "yh")
 push!(xh_01, 3200.)
 push!(xh_02, 3200.)
 push!(yh_01, 3200.)
@@ -96,8 +96,8 @@ println("variance in w_xy: ", var(w_xy_01), " ", var(w_xy_02))
 ## Vertical velocity xy interpolated
 interp = LinearInterpolation((x_02, y_02), w_xy_02, extrapolation_bc=Periodic())
 w_xy_02_to_01 = interp(x_01, y_01)
-println(size(w_xy_02_to_01))
 
+"""
 figure(figsize=(9,4.5))
 subplot(121)
 pcolormesh(xh_01, yh_01, w_xy_01', vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic)
@@ -111,17 +111,21 @@ ylabel("y (m)")
 
 tight_layout()
 display(gcf())
+"""
 
 
 ## Scatter xy
+w_min = max(minimum(w_xy_01), minimum(w_xy_02_to_01))
+w_max = min(maximum(w_xy_01), maximum(w_xy_02_to_01))
+
 figure(figsize=(5.5, 4.5))
-hexbin(vec(w_xy_01), vec(w_xy_02_to_01), gridsize=40, bins="log", cmap=plt.cm.viridis)
+hexbin(vec(w_xy_01), vec(w_xy_02_to_01), gridsize=60, bins="log", cmap=plt.cm.cividis)
 colorbar()
-plot([-2, 5], [-2, 5], "k:")
-xlim(-1.5, 3)
-ylim(-1.5, 3)
-xlabel("w_xy_01 (m s-1)")
-ylabel("w_xy_02 (m s-1)")
+plot([-2, 5], [-2, 5], "w:")
+xlim(w_min, w_max)
+ylim(w_min, w_max)
+xlabel(L"w_xy_01 (m s$^{-1}$)")
+ylabel(L"w_xy_02 (m s$^{-1}$)")
 
 tight_layout()
 display(gcf())
@@ -139,12 +143,12 @@ push!(zp_02, 3200.)
 
 figure(figsize=(9,4.5))
 subplot(121)
-pcolormesh(xh_01, zh_01, w_xz_01', vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic)
+pcolormesh(xh_01, zp_01, w_xz_01', vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic)
 xlabel("x (m)")
 ylabel("y (m)")
 
 subplot(122)
-pcolormesh(xh_02, zh_02, w_xz_02', vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic)
+pcolormesh(xh_02, zp_02, w_xz_02', vmin=-1.5, vmax=1.5, cmap=plt.cm.seismic)
 xlabel("x (m)")
 ylabel("y (m)")
 
