59ffe95c945dfd8e523eaa11a8d09cc104185fe0
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-01-21 14:30:03 +0100

    Added MPI-IO as a reference.

--- test/mpi_write_3d.jl
+++ test/mpi_write_3d.jl
@@ -27,7 +27,7 @@ print("$id: has parallel HDF $(HDF5.has_parallel())\n")
 a = ones(imax, jmax, ktot) * id
 
 
-function write_3d(a)
+function write_3d_hdf(a)
     fid = h5open("test_mpi.h5", "w", commxy, info)
     data_all = create_dataset(fid, "a", datatype(eltype(a)), dataspace((itot, jtot, ktot)), dxpl_mpio=HDF5.H5FD_MPIO_COLLECTIVE)
     is = id_x*imax + 1; ie = (id_x+1)*imax
@@ -37,24 +37,41 @@ function write_3d(a)
     close(fid)
 end
 
-function write_2d(a)
-    fid = h5open("test_mpi_2d.h5", "w", commx, info)
-    if id_y == 0
-        data_all = create_dataset(fid, "a2d", datatype(eltype(a)), dataspace((itot, jtot)), dxpl_mpio=HDF5.H5FD_MPIO_COLLECTIVE)
-        is = id_x*imax + 1; ie = (id_x+1)*imax
-        js = id_y*jmax + 1; je = (id_y+1)*jmax
-        data_all[is:ie, js:je] = a[:, :, 1]
-    end
-    MPI.Barrier(commx)
+
+function write_3d_mpi(a)
+    is = id_x*imax + 1; ie = (id_x+1)*imax
+    js = id_y*jmax + 1; je = (id_y+1)*jmax
+    subarray = MPI.Types.create_subarray((itot, jtot, ktot), (imax, jmax, ktot), (is-1, js-1, 0), MPI.Datatype(eltype(a)))
+    MPI.Types.commit!(subarray)
+    fid = MPI.File.open(commxy, "test_mpi.bin"; read=false, create=true, write=true, uniqueopen=true)
+    MPI.File.set_view!(fid, 0, MPI.Datatype(eltype(a)), subarray, "native")
+    MPI.File.write_all(fid, a[:, :, :])
     close(fid)
 end
 
 
-@time write_3d(a)
-@time write_3d(a)
-@time write_3d(a)
-@time write_2d(a)
-@time write_2d(a)
-@time write_2d(a)
+# function write_2d(a)
+#     fid = h5open("test_mpi_2d.h5", "w", commx, info)
+#     if id_y == 0
+#         data_all = create_dataset(fid, "a2d", datatype(eltype(a)), dataspace((itot, jtot)), dxpl_mpio=HDF5.H5FD_MPIO_COLLECTIVE)
+#         is = id_x*imax + 1; ie = (id_x+1)*imax
+#         js = id_y*jmax + 1; je = (id_y+1)*jmax
+#         data_all[is:ie, js:je] = a[:, :, 1]
+#     end
+#     MPI.Barrier(commx)
+#     close(fid)
+# end
+
+
+@time write_3d_hdf(a)
+@time write_3d_hdf(a)
+@time write_3d_hdf(a)
+MPI.Barrier(commxy)
+@time write_3d_mpi(a)
+@time write_3d_mpi(a)
+@time write_3d_mpi(a)
+# @time write_2d(a)
+# @time write_2d(a)
+# @time write_2d(a)
 
 MPI.Finalize()
