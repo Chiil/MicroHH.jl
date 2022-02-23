fb59ac4026752a28af85d941346c481db09d0e8f
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-02-23 22:37:53 +0100

    Writing arrays works well.

--- test/stats.jl
+++ test/stats.jl
@@ -13,7 +13,7 @@ function add_record!(fid, a, a_name, dims, iter)
     # Check if record variables are available.
     if !haskey(fid, "iter")
         @info "Iter does not exist, incrementing size and adding iter"
-        iterid = create_dataset(fid, "iter", datatype(typeof(iter)), ((1,), (-1,)), chunk=(1,))
+        iterid = create_dataset(fid, "iter", datatype(eltype(iter)), ((1,), (-1,)), chunk=(1,))
         iterid[1] = iter
     else
         iterid = fid["iter"]
@@ -29,14 +29,65 @@ function add_record!(fid, a, a_name, dims, iter)
             iterid[end] = iter
         end
     end
+
+    # Iter is available, continue to store data.
+    if !haskey(fid, a_name)
+        # Create variable.
+        @info "Variable $a_name does not exist, incrementing size and adding variable"
+        aid = create_dataset(fid, a_name, datatype(eltype(a)), ((size(a)..., 1,), (size(a)..., -1,)), chunk=(size(a)..., 1,))
+
+        if dims == nothing
+            HDF5.h5ds_set_scale(fid["time"], "time")
+        else
+            # Add dimensions to variable.
+            for i in 1:length(dims)
+                index_c = length(dims)-i
+                HDF5.h5ds_attach_scale(fid[a_name], fid[dims[i]], index_c)
+            end
+        end
+
+        # Save the first data.
+        @info "Saving data..."
+        r = [ 1:lastindex(a, i) for i in 1:ndims(a) ]
+        aid[r..., end] = a[r...]
+        return nothing
+    else
+        aid = fid[a_name]
+    end
+
+    if size(aid)[end] == size(iterid)[end]-1
+        @info "Saving data..."
+        adims = [ size(aid)... ]
+        adims[end] += 1
+        HDF5.set_extent_dims(aid, tuple(adims...))
+        r = [ 1:lastindex(a, i) for i in 1:ndims(a) ]
+        aid[r..., end] = a[r...]
+    else
+        @info "OHOH DATA IS ALREADY THERE!"
+    end
+
+    close(aid)
+    close(iterid)
 end
 
 
 ## Save a field
-time = 0.
-
 fid = open_stats("test.h5")
-add_record!(fid, time, "time", :t, 0)
-add_record!(fid, time, "time", :t, 10)
-add_record!(fid, time, "time", :t, 300)
+
+time = 0.
+a = rand()
+
+add_record!(fid, time, "time", nothing, 0)
+add_record!(fid, a, "a", ("time",), 0)
+
+time += 10.
+a = rand()
+add_record!(fid, time, "time", nothing, 10)
+add_record!(fid, a, "a", ("time",), 10)
+
+time += 300.
+a = rand()
+add_record!(fid, time, "time", nothing, 300)
+add_record!(fid, a, "a", ("time",), 300)
+
 close(fid)
