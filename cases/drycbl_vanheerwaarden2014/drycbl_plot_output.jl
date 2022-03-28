12ac078036a478bb658505b2f7a46ba682708107
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-03-28 14:38:31 +0200

    Improved plotting script.

--- cases/drycbl_vanheerwaarden2014/drycbl_plot_output.jl
+++ cases/drycbl_vanheerwaarden2014/drycbl_plot_output.jl
@@ -14,13 +14,14 @@ xh = [ h5read("drycbl_cross.h5", "xh")[:]; 2.]
 zh = [ h5read("drycbl_cross.h5", "zh")[:]; 2.]
 s = h5read("drycbl_cross.h5", "s_xz")
 
+
 ## Surface temperature figure.
-for i in 50:20:size(s, 3)
+for i in 50:size(s, 3)
     println("Processing frame $i")
     s_prime = s[:, :, i] .- mean(s[:, :, i], dims=1)
 
     fig = figure(figsize=(12.8, 4.0))
-    pcolormesh(xh, zh, s_prime', vmin=-0.2, vmax=0.3, cmap=plt.cm.cividis)
+    pcolormesh(xh, zh, s_prime', vmin=-0.2, vmax=0.35, cmap=plt.cm.cividis)
     xlabel("x / δ")
     ylabel("z / δ")
     xlim(0., 2.0)
@@ -29,7 +30,7 @@ for i in 50:20:size(s, 3)
     tight_layout()
     title("MicroHH.jl")
 
-    filename = @sprintf("figs/fig_%05i.png", i)
-    savefig(filename, dpi=100)
+    filename = @sprintf("figs/frame_%05i.png", i)
+    savefig(filename, dpi=200)
     close(fig)
 end
