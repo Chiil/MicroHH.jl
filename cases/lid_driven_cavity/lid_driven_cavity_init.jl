17b45132bf3b97428bd6c1fb7bdf4c168cc05f1c
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2022-11-19 13:09:48 +0100

    Add small perturbations to enable testing pressure solver.

--- cases/lid_driven_cavity/lid_driven_cavity_init.jl
+++ cases/lid_driven_cavity/lid_driven_cavity_init.jl
@@ -17,15 +17,18 @@ m = Model("lid_driven_cavity", n_domains, settings, float_type)
 f = m.fields[1]; g = m.grid[1]
 u = @view f.u[g.is+1:g.ie, g.js:g.je, g.ks:g.ke]
 w = @view f.w[g.is:g.ie, g.js:g.je, g.ks+1:g.ke]
-u .+= rand(size(u))
-w .+= rand(size(w))
+u .+= 1e-4 .* rand(float_type, size(u))
+w .+= 1e-4 .* rand(float_type, size(w))
+u .-= mean(u, dims=[1, 2])
+w .-= mean(w, dims=[1, 2])
 
 u_bot = @view f.u_bot[g.is:g.ie, g.js:g.je]
 u_top = @view f.u_top[g.is:g.ie, g.js:g.je]
 v_bot = @view f.v_bot[g.is:g.ie, g.js:g.je]
 v_top = @view f.v_top[g.is:g.ie, g.js:g.je]
 f.u_bot[:, :] .= 0.
-f.u_top[:, :] .= 1.
+# f.u_top[:, :] .= 1.
+f.u_top[:, :] .= 0.
 f.v_bot[:, :] .= 0.
 f.v_top[:, :] .= 0.
 
