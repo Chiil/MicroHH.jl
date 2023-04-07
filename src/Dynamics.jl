00d1a1dad7b34856fd370d35cd61027c27f80f8b
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2023-04-07 09:43:39 +0200

    Split dynamics into advection / diffusion / buoyancy.

--- src/Dynamics.jl
+++ src/Dynamics.jl
@@ -2,70 +2,146 @@ using .StencilBuilder
 
 
 ## Kernels.
-function dynamics_u_kernel!(
+function advection_u_kernel!(
     ut, u, v, w,
-    visc,
-    dxi, dyi, dzi, dzhi,
+    dxi, dyi, dzi,
     is, ie, js, je, ks, ke)
 
     @fast3d begin
         @fd (ut[ih, j, k], u[ih, j, k], v[i, jh, k], w[i, j, kh]) begin
             ut += (
-                - gradx(interpx(u) * interpx(u)) + visc * (gradx(gradx(u)))
-                - grady(interpx(v) * interpy(u)) + visc * (grady(grady(u)))
-                - gradz(interpx(w) * interpz(u)) + visc * (gradz(gradz(u))) )
+                - gradx(interpx(u) * interpx(u))
+                - grady(interpx(v) * interpy(u))
+                - gradz(interpx(w) * interpz(u)) )
         end
     end
 end
 
 
-function dynamics_v_kernel!(
-    vt, u, v, w,
+function diffusion_u_kernel!(
+    ut, u,
     visc,
     dxi, dyi, dzi, dzhi,
+    is, ie, js, je, ks, ke)
+
+    @fast3d begin
+        @fd (ut[ih, j, k], u[ih, j, k]) begin
+            ut += (
+                + visc * (gradx(gradx(u)))
+                + visc * (grady(grady(u)))
+                + visc * (gradz(gradz(u))) )
+        end
+    end
+end
+
+
+function advection_v_kernel!(
+    vt, u, v, w,
+    dxi, dyi, dzi,
     is, ie, js, je, ks, ke)
 
     @fast3d begin
         @fd (vt[i, jh, k], u[ih, j, k], v[i, jh, k], w[i, j, kh]) begin
             vt += (
-                - gradx(interpy(u) * interpx(v)) + visc * (gradx(gradx(v)))
-                - grady(interpy(v) * interpy(v)) + visc * (grady(grady(v)))
-                - gradz(interpy(w) * interpz(v)) + visc * (gradz(gradz(v))) )
+                - gradx(interpy(u) * interpx(v))
+                - grady(interpy(v) * interpy(v))
+                - gradz(interpy(w) * interpz(v)) )
         end
     end
 end
 
 
-function dynamics_w_kernel!(
-    wt, u, v, w, s, s_ref,
-    visc, alpha,
-    dxi, dyi, dzi, dzhi,
-    is, ie, js, je, ks, ke) # CvH: ke represents keh here, but that is not yet supported by fast3d, needs fixing.
-
-    @fast3d begin
-        @fd (wt[i, j, kh], u[ih, j, k], v[i, jh, k], w[i, j, kh], s[i, j, k], s_ref[k]) begin
-            wt += (
-                - gradx(interpz(u) * interpx(w)) + visc * (gradx(gradx(w)))
-                - grady(interpz(v) * interpy(w)) + visc * (grady(grady(w)))
-                - gradz(interpz(w) * interpz(w)) + visc * (gradz(gradz(w)))
-                + alpha*interpz(s - s_ref) )
-        end
-    end
-end
-
-
-function dynamics_s_kernel!(
-    st, u, v, w, s,
+function diffusion_v_kernel!(
+    vt, v,
     visc,
     dxi, dyi, dzi, dzhi,
     is, ie, js, je, ks, ke)
 
     @fast3d begin
+        @fd (vt[i, jh, k], v[i, jh, k]) begin
+            vt += (
+                + visc * (gradx(gradx(v)))
+                + visc * (grady(grady(v)))
+                + visc * (gradz(gradz(v))) )
+        end
+    end
+end
+
+
+function advection_w_kernel!(
+    wt, u, v, w,
+    dxi, dyi, dzhi,
+    is, ie, js, je, ks, ke) # CvH: ke represents keh here, but that is not yet supported by fast3d, needs fixing.
+
+    @fast3d begin
+        @fd (wt[i, j, kh], u[ih, j, k], v[i, jh, k], w[i, j, kh]) begin
+            wt += (
+                - gradx(interpz(u) * interpx(w))
+                - grady(interpz(v) * interpy(w))
+                - gradz(interpz(w) * interpz(w)) )
+        end
+    end
+end
+
+
+function diffusion_w_kernel!(
+    wt, w,
+    visc,
+    dxi, dyi, dzi, dzhi,
+    is, ie, js, je, ks, ke) # CvH: ke represents keh here, but that is not yet supported by fast3d, needs fixing.
+
+    @fast3d begin
+        @fd (wt[i, j, kh], w[i, j, kh]) begin
+            wt += (
+                + visc * (gradx(gradx(w)))
+                + visc * (grady(grady(w)))
+                + visc * (gradz(gradz(w))) )
+        end
+    end
+end
+
+
+function buoyancy_w_kernel!(
+    wt, s,
+    s_ref, alpha,
+    is, ie, js, je, ks, ke) # CvH: ke represents keh here, but that is not yet supported by fast3d, needs fixing.
+
+    @fast3d begin
+        @fd (wt[i, j, kh], s[i, j, k], s_ref[k]) begin
+            wt += alpha*interpz(s - s_ref)
+        end
+    end
+end
+
+
+function advection_s_kernel!(
+    st, u, v, w, s,
+    dxi, dyi, dzi,
+    is, ie, js, je, ks, ke)
+
+    @fast3d begin
         @fd (st[i, j, k], u[ih, j, k], v[i, jh, k], w[i, j, kh], s[i, j, k]) begin
             st += (
-                - gradx(u * interpx(s)) + visc * (gradx(gradx(s)))
-                - grady(v * interpy(s)) + visc * (grady(grady(s)))
-                - gradz(w * interpz(s)) + visc * (gradz(gradz(s))) )
+                - gradx(u * interpx(s))
+                - grady(v * interpy(s))
+                - gradz(w * interpz(s)) )
+        end
+    end
+end
+
+
+function diffusion_s_kernel!(
+    st, s,
+    visc,
+    dxi, dyi, dzi, dzhi,
+    is, ie, js, je, ks, ke)
+
+    @fast3d begin
+        @fd (st[i, j, k], s[i, j, k]) begin
+            st += (
+                + visc * (gradx(gradx(s)))
+                + visc * (grady(grady(s)))
+                + visc * (gradz(gradz(s))) )
         end
     end
 end
@@ -73,27 +149,54 @@ end
 
 ## Dynamics kernels launcher.
 function calc_dynamics_tend!(f::Fields, g::Grid, to::TimerOutput)
-    @timeit to "dynamics_u_kernel" dynamics_u_kernel!(
+    @timeit to "advection_u_kernel" advection_u_kernel!(
         f.u_tend, f.u, f.v, f.w,
+        g.dxi, g.dyi, g.dzi,
+        g.is, g.ie, g.js, g.je, g.ks, g.ke)
+
+    @timeit to "diffusion_u_kernel" diffusion_u_kernel!(
+        f.u_tend, f.u,
         f.visc,
         g.dxi, g.dyi, g.dzi, g.dzhi,
         g.is, g.ie, g.js, g.je, g.ks, g.ke)
 
-    @timeit to "dynamics_v_kernel" dynamics_v_kernel!(
+    @timeit to "advection_v_kernel" advection_v_kernel!(
         f.v_tend, f.u, f.v, f.w,
+        g.dxi, g.dyi, g.dzi,
+        g.is, g.ie, g.js, g.je, g.ks, g.ke)
+
+    @timeit to "diffusion_v_kernel" diffusion_v_kernel!(
+        f.v_tend, f.v,
         f.visc,
         g.dxi, g.dyi, g.dzi, g.dzhi,
         g.is, g.ie, g.js, g.je, g.ks, g.ke)
 
-    @timeit to "dynamics_w_kernel" dynamics_w_kernel!(
-        f.w_tend, f.u, f.v, f.w, f.s, f.s_ref,
-        f.visc, f.alpha,
+    @timeit to "advection_w_kernel" advection_w_kernel!(
+        f.w_tend, f.u, f.v, f.w,
+        g.dxi, g.dyi, g.dzhi,
+        # g.is, g.ie, g.js, g.je, g.ks, g.keh) # CvH temporary hack
+        g.is, g.ie, g.js, g.je, g.ks+1, g.keh-1)
+
+    @timeit to "diffusion_w_kernel" diffusion_w_kernel!(
+        f.w_tend, f.w,
+        f.visc,
         g.dxi, g.dyi, g.dzi, g.dzhi,
         # g.is, g.ie, g.js, g.je, g.ks, g.keh) # CvH temporary hack
         g.is, g.ie, g.js, g.je, g.ks+1, g.keh-1)
 
-    @timeit to "dynamics_s_kernel" dynamics_s_kernel!(
+    @timeit to "buoyancy_w_kernel" buoyancy_w_kernel!(
+        f.w_tend, f.s,
+        f.s_ref, f.alpha,
+        # g.is, g.ie, g.js, g.je, g.ks, g.keh) # CvH temporary hack
+        g.is, g.ie, g.js, g.je, g.ks+1, g.keh-1)
+
+    @timeit to "advection_s_kernel" advection_s_kernel!(
         f.s_tend, f.u, f.v, f.w, f.s,
+        g.dxi, g.dyi, g.dzi,
+        g.is, g.ie, g.js, g.je, g.ks, g.ke)
+
+    @timeit to "diffusion_s_kernel" diffusion_s_kernel!(
+        f.s_tend, f.s,
         f.visc,
         g.dxi, g.dyi, g.dzi, g.dzhi,
         g.is, g.ie, g.js, g.je, g.ks, g.ke)
